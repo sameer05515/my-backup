e550378c1115540b1286da457357cedaede63483
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2023-11-17 12:11:16 +0530

    commit

--- 17-Nov-2023/mongo-export.bat
+++ 17-Nov-2023/mongo-export.bat
@@ -1,24 +1,24 @@
 @echo off
-set outputFolder=C:\Prem\GIT\my-backup\17-Nov-2023
+set outputFolder=C:\Prem\GIT\my-backup\17-Nov-2023\json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=activities --out=activities.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=activities --out=%outputFolder%\activities.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=comparableobjectmodels --out=comparableobjectmodels.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=comparableobjectmodels --out=%outputFolder%\comparableobjectmodels.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=datas --out=datas.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=datas --out=%outputFolder%\datas.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=products --out=products.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=products --out=%outputFolder%\products.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tags --out=tags.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tags --out=%outputFolder%\tags.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tasks --out=tasks.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tasks --out=%outputFolder%\tasks.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=topics --out=topics.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=topics --out=%outputFolder%\topics.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tweets --out=tweets.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=tweets --out=%outputFolder%\tweets.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=usercolls --out=usercolls.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=usercolls --out=%outputFolder%\usercolls.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=users --out=users.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=users --out=%outputFolder%\users.json
 
-mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=words --out=words.json
+mongoexport --uri=mongodb://localhost:27017/mongodb_test --collection=words --out=%outputFolder%\words.json
