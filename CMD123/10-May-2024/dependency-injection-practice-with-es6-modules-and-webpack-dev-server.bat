49778c5e8d3e121657d5d7adb1b379a4ab4f5ef9
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-09-13 04:55:23 +0530

    commit

---
+++ CMD123/10-May-2024/dependency-injection-practice-with-es6-modules-and-webpack-dev-server.bat
@@ -0,0 +1,12 @@
+@echo off
+
+title "dependency-injection-practice-with-es6-modules-and-webpack-dev-server"
+
+cd D:\GIT\unit-testing-playground\example-base-03\dependency-injection-practice-with-es6-modules-and-webpack-dev-server
+
+D:
+
+@REM npm run start-my-webpack-to-build
+npm run start-dev
+
+
