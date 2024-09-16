586d9c3d5fc871eee2148fe125470d021a3345df
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-09-16 14:00:46 +0530

    commit

---
+++ CMD123/10-May-2024/next-ts-basics.bat
@@ -0,0 +1,10 @@
+@echo off
+
+title "D:\GIT\react-projects\learn-react\react-ts-basics"
+
+cd D:\GIT\react-projects\learn-react-and-next-ts\next-ts-basics
+d:
+
+@REM npm run dev -- -p 5174
+
+npm run dev
