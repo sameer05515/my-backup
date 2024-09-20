4527484806e8edfdca4d8e590c61519d1db8da99
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-09-20 12:34:49 +0530

    commit

---
+++ CMD123/10-May-2024/promise-practice-in-react.bat
@@ -0,0 +1,8 @@
+@echo off
+
+title "D:\GIT\unit-testing-playground\example-base-03\promise-practice-in-react"
+
+cd D:\GIT\unit-testing-playground\example-base-03\promise-practice-in-react
+d:
+
+npm start
