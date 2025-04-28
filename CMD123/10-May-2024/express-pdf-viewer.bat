b724005b89396eb4dde5b1ff38ab92a8221cae3c
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-04-28 11:19:21 +0530

    commit

---
+++ CMD123/10-May-2024/express-pdf-viewer.bat
@@ -0,0 +1,10 @@
+@REM D:\GIT\unit-testing-playground\example-base-05\express-pdf-viewer
+
+
+@echo off
+
+title "express-pdf-viewer"
+set CURRENT_DIR=%cd%
+cd D:\GIT\unit-testing-playground\example-base-05\express-pdf-viewer
+D:
+npm run dev:v2
