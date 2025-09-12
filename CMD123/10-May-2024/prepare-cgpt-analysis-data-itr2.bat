3cc7d8fda59be75e88ae0c02a779f9a5bfe2efca
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-09-12 19:40:52 +0530

    commit

---
+++ CMD123/10-May-2024/prepare-cgpt-analysis-data-itr2.bat
@@ -0,0 +1,9 @@
+@echo off
+
+title "Prepare CGPT Analysis data: itr2"
+set CURRENT_DIR=%cd%
+
+cd D:\GIT\unit-testing-playground\example-base-05\express-pdf-viewer\src\common\CgptSnapshot\prepare-analysis-data
+d:
+
+node .\index.itr2.js
