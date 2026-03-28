07bc9ed1b4299c74264b1c72f7fde4e149286c3b
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2026-03-28 11:31:09 +0530

    Remove deprecated Kafka and Redis demo files, including tutorials, Docker configurations, and application code, to streamline the project structure and eliminate unused components.

--- redis/redis-kafka-demo-sb/src/main/java/com/example/demo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TodoRepository extends MongoRepository<Todo, String> {
-}
--- swagger/tutorials.md
+++
@@ -1,4 +0,0 @@
-- [How to Integrate Swagger in Spring Boot | Step-by-Step Tutorial](https://www.youtube.com/watch?v=7QlvGBJ9Tng)
-
-
-- swagger ui will be available on `http://localhost:8080/swagger-ui/index.html`
